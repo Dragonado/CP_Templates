9ee8843decc612805023caf80aca5feb31356e87
Chaithanya Shyam D <codecraft15@gmail.com>
2021-01-19 21:23:05 +0530

    Add Fenwick Tree

---
+++ SumFenwickTree.cpp
@@ -0,0 +1,98 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp> //required
+//#include <ext/pb_ds/tree_policy.hpp> //required
+
+//using namespace __gnu_pbds; //required 
+using namespace std;
+//template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; 
+
+// ordered_set <int> s;
+// s.find_by_order(k); returns the (k+1)th smallest element
+// s.order_of_key(k); returns the number of elements in s strictly less than k
+
+#define MOD              (1000000000+7) // change as required
+#define pb(x)            push_back(x)
+#define mp(x,y)          make_pair(x,y)
+#define all(x)           x.begin(), x.end()
+#define print(vec,l,r)   for(int i = l; i <= r; i++) cout << vec[i] <<" "; cout << endl;
+#define input(vec,N)     for(int i = 0; i < (N); i++) cin >> vec[i];
+#define debug(x)         cerr << #x << " = " << (x) << endl;
+#define leftmost_bit(x)  (63-__builtin_clzll(x))
+#define rightmost_bit(x) __builtin_ctzll(x) // count trailing zeros
+#define set_bits(x)      __builtin_popcountll(x) 
+#define pow2(i)          (1LL << (i))
+#define is_on(x, i)      ((x) & pow2(i)) // state of the ith bit in x
+#define set_on(x, i)     ((x) | pow2(i)) // returns integer x with ith bit on
+#define set_off(x, i)    ((x) & ~pow2(i)) // returns integer x with ith bit off
+  
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef long long int ll;
+
+// highly risky #defines
+#define int ll // disable when you want to make code a bit faster
+#define endl '\n' // disable when dealing with interactive problems
+
+// taken from: https://cp-algorithms.com/data_structures/fenwick.html
+struct FenwickTree {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n, 0);
+    }
+
+    FenwickTree(vector<int> a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int r) {
+        int ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+        return ret;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += delta;
+    }
+};
+
+
+void solve(){
+	// code starts from here
+	
+}
+
+
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+signed main(){
+ 	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	//startTime = clock();
+	// mt19937_64 rnd(time(NULL));
+	
+	int T = 1;
+	cin >> T;
+
+	while(T--){
+		solve();
+	}
+	
+	//cerr << getCurrentTime() << endl;
+	return 0;
+}
