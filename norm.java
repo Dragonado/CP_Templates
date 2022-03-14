dc0c47afb07f7828e621dcca630ceb9de33806ab
Chaithanya Shyam D <codecraft15@gmail.com>
2022-03-14 19:28:05 +0530

    added java template

---
+++ norm.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+ 
+public class norm{
+
+    static FastScanner fs = new FastScanner();
+    public static void main(String args[]){
+        int T = fs.nextInt();
+        for(int t = 0; t < T; t++){
+            solve();
+        }
+    }
+
+    public static void solve(){
+        // code starts from here
+        
+    }
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
